5a16d84161ac9a4187d7431068029bbe20e9377a
pensuke628 <ryz123212327@gmail.com>
2024-06-30 22:52:04 +0900

    feat: vpcを追加

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    new ec2.Vpc(this, 'TestVpc', {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'TestPrivate',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
   }
 }
 
